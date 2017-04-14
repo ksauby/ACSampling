e031104ddb2b9ce224688d2d7253af9c965abe0d
ksauby <ksauby@gmail.com>
2017-04-14 11:34:25 -0400

    refinement of testing of pi_ij_RACS

--- tests/testthat/test_pi_ij_RACS.R
+++ tests/testthat/test_pi_ij_RACS.R
@@ -7,19 +7,22 @@ test_that("R_hat, Horvitz-Thompson Ratio Estimator, without replacement", {
 	pi_ij_RACS_no_mthresh 	<- pi_ij(N,n1,m)
 	pi_ij_RACS_mthresh_4 	<- pi_ij_RACS(N,n1,m,m_threshold)
 
-	# ARE PI_IJ FORMULAS EQUIVALENT WHEN ALL M ARE BELOW M_THRESHOLD
-	
+	# ARE PI_IJ FORMULAS EQUIVALENT WHEN ALL M ARE BELOW M_THRESHOLD?
+	expect_that(
+		pi_ij_RACS_mthresh_6,
+		 equals(pi_ij_RACS_no_mthresh)	
+	)
 	# MANUALLY CALCULATE AND CHECK VALUES IN MATRIX
 	# 3 is not over m_threshold
 	# 3 is not over m_threshold
 	expect_that(
 		(vec = as.vector(pi_ij_RACS_mthresh_4[6:8,6:8])),
-		 is_equivalent_to(
+		 equals(
 			rep(
 				1 - (
-					choose(N-m[6],n1) +
-					choose(N-m[6],n1) - 
-					choose(N-m[6]-m[6],n1)
+					choose(N-3,n1) +
+					choose(N-3,n1) - 
+					choose(N-3-3,n1)
 				) / choose(N,n1),
 				length(vec)
 			)
@@ -32,12 +35,12 @@ test_that("R_hat, Horvitz-Thompson Ratio Estimator, without replacement", {
 			pi_ij_RACS_mthresh_4[1:5,6:8],
 			pi_ij_RACS_mthresh_4[6:8,1:5]
 		)),
-		 is_equivalent_to(
+		 equals(
 			rep(
 				1 - (
 					choose(N-m_threshold,n1) +
-					choose(N-m[6],n1) - 
-					choose(N-m_threshold-m[6],n1)
+					choose(N-3,n1) - 
+					choose(N-m_threshold-3,n1)
 				) / choose(N,n1),
 				length(vec)
 			)
@@ -50,12 +53,12 @@ test_that("R_hat, Horvitz-Thompson Ratio Estimator, without replacement", {
 			pi_ij_RACS_mthresh_4[1:5,9:13],
 			pi_ij_RACS_mthresh_4[9:13,1:5]
 		)),
-		 is_equivalent_to(
+		 equals(
 			rep(
 				1 - (
 					choose(N-m_threshold,n1) +
-					choose(N-m[9],n1) - 
-					choose(N-m_threshold-m[9],n1)
+					choose(N-1,n1) - 
+					choose(N-m_threshold-1,n1)
 				) / choose(N,n1),
 				length(vec)
 			)
@@ -68,12 +71,12 @@ test_that("R_hat, Horvitz-Thompson Ratio Estimator, without replacement", {
 			pi_ij_RACS_mthresh_4[6:8,9:13],
 			pi_ij_RACS_mthresh_4[9:13,6:8]
 		)),
-		 is_equivalent_to(
+		 equals(
 			rep(
 				1 - (
-					choose(N-m[6],n1) +
-					choose(N-m[9],n1) - 
-					choose(N-m[6]-m[9],n1)
+					choose(N-3,n1) +
+					choose(N-1,n1) - 
+					choose(N-3-1,n1)
 				) / choose(N,n1),
 				length(vec)
 			)
@@ -83,12 +86,12 @@ test_that("R_hat, Horvitz-Thompson Ratio Estimator, without replacement", {
 	# 1 is not over the threshold	
 	expect_that(
 		(vec = as.vector(pi_ij_RACS_mthresh_4[9:13,9:13])),
-		 is_equivalent_to(
+		 equals(
 			rep(
 				1 - (
-					choose(N-m[9],n1) +
-					choose(N-m[9],n1) - 
-					choose(N-m[9]-m[9],n1)
+					choose(N-1,n1) +
+					choose(N-1,n1) - 
+					choose(N-1-1,n1)
 				) / choose(N,n1),
 				length(vec)
 			)
