04b150c840554b2dcd0c64d71c36dae2ddc8fcdf
ksauby <ksauby@gmail.com>
2017-04-16 13:23:11 -0400

    generalize function and calculate spatial statistics

--- R/calculatePopulationSummaryStatistics.R
+++ R/calculatePopulationSummaryStatistics.R
@@ -2,7 +2,7 @@
 #' 
 #' @param population_data Data on multiple realizations of patches of the species of interest within the grid of locations (created by \code{createSpeciesPatchPopulations} function).
 #' @param summary.variables Vector of variables for which summary statistics should be calculated.
-#' @param grouping.variables Categorical variables identifying the patch realization from which the simulation data was generated (e.g., \code{n.networks} and \code{realization}).
+#' @param population.grouping.variable Categorical variable identifying the different populations.
 #' @param ratio.variables Variables for which to use ratio estimators
 #' @return Dataframe including summary statistics for each column identified in \code{summary.variables} and for each category identified in \code{grouping.variables}.
 #' @examples
@@ -66,12 +66,12 @@ calculatePopulationSummaryStatistics <- function(
 	population_data, 
 	summary.variables, 
 	ratio.variables=NULL, 
-	grouping.variables
+	population.grouping.variable
 ) {
 	funs <- . <- m <- NetworkID <- NULL
-	# for each grouping.variables combo, calculate mean, var, sum
+	# for each population.grouping.variable combo, calculate mean, var, sum
 	X = population_data %>%
-		group_by_(.dots=lapply(grouping.variables, as.symbol)) %>%
+		group_by_(.dots=population.grouping.variable) %>%
 		dplyr::select_(.dots=lapply(
 			c(
 				summary.variables, 
@@ -82,7 +82,9 @@ calculatePopulationSummaryStatistics <- function(
 		summarise_each(
 			funs(
 				mean(., na.rm=T), 
-				sum(., na.rm=T)
+				sum(., na.rm=T),
+				PopVariance,
+				popCV
 			)
 		) %>%
 		ungroup
@@ -131,12 +133,15 @@ calculatePopulationSummaryStatistics <- function(
 			)
 		}
 	}
-	# for each grouping.variables combo, calculate summary statistics for m and number of species patches
+	# for each population.grouping.variable combo, calculate summary statistics for m and number of species patches
 	# this calculates the m statistics for the unique Network sizes
 	Y1 = population_data %>%
-		group_by_(.dots=lapply(c("NetworkID", grouping.variables), as.symbol)) %>%
+		group_by_(.dots=lapply(
+			c("NetworkID", population.grouping.variable), 
+			as.symbol
+		)) %>%
 		summarise(m = m[1]) %>%
-		group_by_(.dots=lapply(grouping.variables, as.symbol)) %>%
+		group_by_(.dots=population.grouping.variable) %>%
 		summarise(
 			m_min_unique_neigh = min(m),
 			m_max_unique_neigh = max(m),
@@ -148,7 +153,7 @@ calculatePopulationSummaryStatistics <- function(
 		as.data.frame
 	# this calculates the m statistics for all units
 	Y2 = population_data %>%
-		group_by_(.dots=lapply(grouping.variables, as.symbol)) %>%
+		group_by_(.dots=population.grouping.variable) %>%
 		summarise(
 			m_min = min(m),
 			m_max = max(m),
@@ -157,13 +162,54 @@ calculatePopulationSummaryStatistics <- function(
 		) %>%
 		ungroup %>%
 		as.data.frame
-	Z = population_data %>%
-		group_by_(.dots=lapply(grouping.variables, as.symbol)) %>%
+	# spatial statistics	
+	for (i in 1:length(unique(eval(parse(text=paste("population_data$", population.grouping.variable, sep="")))))) {
+		temp <- patch_data %>% 
+			filter(
+				## NEED TO DO THIS FOR SAMPLE SPECIES REALIZATIONS AS WELL
+				eval(parse(text=population.grouping.variable))) ==
+				unique(eval(parse(
+					text=paste(
+						"population_data$", 
+						population.grouping.variable, 
+						sep=""
+					)
+				)))[i]
+			)
+		coordinates(temp) = ~ x+y
+		for (j in 1:length(summary.variables)) {
+			# variogram information
+			A1 <- autofitVariogram(
+				eval(parse(text=summary.variables[j])) ~ 1,
+				temp
+			)$var_model
+			A2 <- var5_parms[1, ]$psill
+			A3 <- var5_parms[2, ]$psill
+			A4 <- var5_parms[2, ]$range
+			# join counts and moran's i
+			nb <- cell2nb(nrow = 30, ncol = 30)
+			lwb <- nb2listw(nb, style = "S") # convert to weights
+			A5 <- joincount.test(as.factor(
+				temp$eval(parse(text=summary.variables[j]))),
+				lwb
+			)[[2]]$estimate[1]
+			A6 <- moran.test(
+				temp$eval(parse(text=summary.variables[j])), 
+				lwb
+			)$estimate[1]
+		}		
+	}
+	Z
+	
+	
+	
+	A = population_data %>%
+		group_by_(.dots=population.grouping.variable) %>%
 		summarise(N = length(m)) %>%
 		ungroup %>%
 		as.data.frame
-	X %<>% merge(Y1, by=grouping.variables) %>%
-		merge(Y2, by=grouping.variables) %>%
-		merge(Z, by=grouping.variables)	
+	X %<>% merge(Y1, by=population.grouping.variable) %>%
+		merge(Y2, by=population.grouping.variable) %>%
+		merge(A, by=population.grouping.variable)	
 	return(X)
 }
