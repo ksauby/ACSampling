ae9110906788b703b54e45bd690496d0b897f0a2
ksauby <ksauby@gmail.com>
2017-07-28 10:07:03 -0400

    writing var_pi function

--- R/variance_wo_jointinclusionprobabilities.R
+++ R/variance_wo_jointinclusionprobabilities.R
@@ -1,4 +1,23 @@
-total sample size?
+# total sample size?
+
+
+#' # Initiate ACS
+#' Z = createACS(Thompson1990Figure1Population, seed=2, n1=10, "y_value", condition=0)
+
+
+
+var_pi <- function(N, n1, m, y, pi_i_values, alpha_i, z_i) {
+	pi_i_values <- pi_i(N, n1, m)
+	n <- length(y)
+	if (length(y) != length(pi_i_values)) {
+		stop("y and pi_i must be of equal length.")
+	}
+	alpha_i <- eval(parse(text=alpha_i))
+	B_hat <- sum(alpha_i(pi_i_values, n) * z_i * y/pi_i_values) / 
+		sum(alpha_i(pi_i_values, n) * z_i^2)
+	epsilon_i <- y/pi_i_values - B_hat * z_i
+	sum(alpha_i * epsilon_i^2)
+}
 
 Hartley_Rao <- function(pi_i, n) {
 	1 - pi_i * (n - 1)/n
@@ -32,4 +51,4 @@ Deville <- function(pi_i, n) {
 	1 / (1 - pi_i) * (1 - (1/D^2) * A)
 }
 
-Brewer 1 and 2
+# Brewer 1 and 2
