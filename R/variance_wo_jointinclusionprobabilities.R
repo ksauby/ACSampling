f9b5e274c8a4ad442373ec824e158a0d8c25d005
ksauby <ksauby@gmail.com>
2017-07-29 12:41:48 -0400

    wrote description

--- R/variance_wo_jointinclusionprobabilities.R
+++ R/variance_wo_jointinclusionprobabilities.R
@@ -1,56 +1,48 @@
-#' Calculate the variance of the Horvitz-Thompson estimator of the mean
+#' Hakey variance estimator free of joint inclusion probability calculations for unequal probability sampling
 #' @param pi_i_values vector of inclusion probabilities, if not calculated using this function. Default is \code{NULL}.
 #' @param N Population size
 #' @param n1 Initial sample size
 #' @param m Vector of $m$, each corresponding to a unique network.
 #' @param y Vector of $y$ total, each corresponding to a unique network.
-#' @references Sauby, K.E and Christman, M.C. \emph{In preparation.} Restricted adaptive cluster sampling.
-#'
-#' Thompson, S. (1990). Adaptive Cluster Sampling. \emph{Journal of the American Statistical Association}, 85(412): 1050--1059.
-#' @examples 
-#' library(ggplot2)
-#' library(magrittr)
-#' library(dplyr)
+#' @description Option for the "var_pi" function.
+#' @references Hajek, J. (1964). Asymptotic theory of rejective sampling with varying probabilities from a finite population. The Annals of Mathematical Statistics.
+#' Berger, Y. G., & Tille, Y. (2009). Sampling with Unequal Probabilities. Handbook of Statistics, 1–17.
+
 
 Hajek <- function(pi_i, n) {
 	pi_i * (1 - pi_i) * n / (n - 1)
 }
 
 
-#' Calculate the variance of the Horvitz-Thompson estimator of the mean
-#' @param pi_i_values vector of inclusion probabilities, if not calculated using this function. Default is \code{NULL}.
+#' Variance estimator free of joint inclusion probability calculations for unequal probability sampling
 #' @param N Population size
-#' @param n1 Initial sample size
-#' @param m Vector of $m$, each corresponding to a unique network.
-#' @param y Vector of $y$ total, each corresponding to a unique network.
-#' @references Sauby, K.E and Christman, M.C. \emph{In preparation.} Restricted adaptive cluster sampling.
-#'
-#' Thompson, S. (1990). Adaptive Cluster Sampling. \emph{Journal of the American Statistical Association}, 85(412): 1050--1059.
-#' @examples 
-#' library(ggplot2)
-#' library(magrittr)
-#' library(dplyr)
+#' @param y Vector of $y$ values.
+#' @param pi_i_values vector of first-order inclusion probabilities, calculated using \code{pi_i}.
+#' @param estimator Options include "Hartley_Rao", "Hajek", "Rosen", "Berger", and "Deville".
+#' @references Hajek, J. (1964). Asymptotic theory of rejective sampling with varying probabilities from a finite population. The Annals of Mathematical Statistics.
+#' Berger, Y. G., & Tille, Y. (2009). Sampling with Unequal Probabilities. Handbook of Statistics, 1–17.
 
-var_pi <- function(n, pi_i_values, y, alpha_i, lambda_i, z_i) {
+var_pi <- function(n, y, pi_i_values, estimator) {
 	#n <- length(y)
 	#if (length(y) != length(pi_i_values)) {
 	#	stop("y and pi_i must be of equal length.")
 	#}
-	if (alpha_i == "Hartley_Rao") {
-		alpha_i_values <- Hartley_Rao(pi_i_values, n)
-		lambda_i_values <- 1
-	}
-	if (alpha_i == "Hajek") {
+	z_i <- 1
+	#if (estimator == "Hartley_Rao") {
+	#	alpha_i_values <- Hartley_Rao(pi_i_values, n)
+	#	lambda_i_values <- 1
+	#}
+	if (estimator == "Hajek") {
 		lambda_i_values <- alpha_i_values <- Hajek(pi_i_values, n)
 	}
-	if (alpha_i == "Rosen") {
-		lambda_i_values <- alpha_i_values <- Rosen(pi_i_values, n)
-	}
-	if (alpha_i == "Berger") {
-	}
-	if (alpha_i == "Deville") {
-		lambda_i_values <- alpha_i_values <- Deville(pi_i_values, n)
-	}
+	#if (estimator == "Rosen") {
+	#	lambda_i_values <- alpha_i_values <- Rosen(pi_i_values, n)
+	#}
+	#if (estimator == "Berger") {
+	#}
+	#if (estimator == "Deville") {
+	#	lambda_i_values <- alpha_i_values <- Deville(pi_i_values, n)
+	#}
 	#alpha_i <- eval(parse(text=alpha_i))
 	
 	B_hat <- sum(lambda_i_values * z_i * y/pi_i_values) / 
