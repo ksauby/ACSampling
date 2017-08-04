b4e207786fd916c198ef83ec857959349c30424f
ksauby <ksauby@gmail.com>
2017-08-03 21:21:55 -0400

    trying to get testing to run without loading any other packages

--- tests/testthat/summary_functions.R
+++ tests/testthat/summary_functions.R
@@ -122,6 +122,7 @@ test_that("calculatePopulationSummaryStatistics", {
 # ALSO TEST OVAR
 
 test_that("Sampling Bias and Relative Efficiency, population 6, SamplingDesign=ACS, N.SRSWOR.plots==100", {	
+	data(simulation_data)
 	patch_data_summary_wide <- createWidePopulationSummaryStatistics(
 		PopulationSummaryStatistics = CactusRealizationSummary,
 		ovar = "Stricta",
@@ -130,12 +131,12 @@ test_that("Sampling Bias and Relative Efficiency, population 6, SamplingDesign=A
 	)
 	population_6 <- patch_data_summary_wide %>% filter(population==6)
 	# MANUALLY CALCULATE MSE and RE
-	simulation_data <- simdata_all_re %>% filter(
+	temp_sim_data <- simulation_data %>% filter(
 		population==6, 
 		SamplingDesign=="ACS", 
 		N.SRSWOR.plots==100
 	)
-	simulation_data_ratio <- simdata_all_re %>% filter(
+	temp_sim_data_ratio <- simulation_data %>% filter(
 		population==6, 
 		SamplingDesign=="ACS", 
 		N.SRSWOR.plots==100,
@@ -145,60 +146,60 @@ test_that("Sampling Bias and Relative Efficiency, population 6, SamplingDesign=A
 	mean_MSE_Stricta <- sum(
 		(
 			# observed
-			simulation_data$Stricta_mean_observed - 
+			temp_sim_data$Stricta_mean_observed - 
 			# true
 			population_6$Stricta_mean
 		)^2
 	# n simulations
-	)/dim(simulation_data)[1]
+	)/dim(temp_sim_data)[1]
 	RE_Stricta = (
-		population_6$Stricta_var/unique(simulation_data$N.Total.plots_mean) *
-		(1 - unique(simulation_data$N.Total.plots_mean)/population_6$N)
+		population_6$Stricta_var/unique(temp_sim_data$N.Total.plots_mean) *
+		(1 - unique(temp_sim_data$N.Total.plots_mean)/population_6$N)
 	) /	mean_MSE_Stricta
 	# ratio variables
 	mean_MSE_CACA_on_Stricta <- sum(
 		(
 			# observed
-			simulation_data_ratio$CACA_on_Stricta_ratio_mean_observed - 
+			temp_sim_data_ratio$CACA_on_Stricta_ratio_mean_observed - 
 			# true
 			population_6$CACA_on_Stricta_ratio_mean
 		)^2
 	# n simulations
-	)/dim(simulation_data_ratio)[1]
+	)/dim(temp_sim_data_ratio)[1]
 	mean_MSE_MEPR_on_Stricta <- sum(
 		(
 			# observed
-			simulation_data_ratio$MEPR_on_Stricta_ratio_mean_observed - 
+			temp_sim_data_ratio$MEPR_on_Stricta_ratio_mean_observed - 
 			# true
 			population_6$MEPR_on_Stricta_ratio_mean
 		)^2
 	# n simulations
-	)/dim(simulation_data_ratio)[1]
+	)/dim(temp_sim_data_ratio)[1]
 	mean_MSE_Old_Moth_Evidence_Stricta <- sum(
 		(
 			# observed
-			simulation_data_ratio$Old_Moth_Evidence_Stricta_ratio_mean_observed - 
+			temp_sim_data_ratio$Old_Moth_Evidence_Stricta_ratio_mean_observed - 
 			# true
 			population_6$Old_Moth_Evidence_Stricta_ratio_mean
 		)^2
 	# n simulations
-	)/dim(simulation_data_ratio)[1]
+	)/dim(temp_sim_data_ratio)[1]
 	RE_CACA_on_Stricta = (
-		population_6$CACA_on_Stricta_ratio_var/unique(simulation_data_ratio$N.Total.plots_mean) *
-		(1 - unique(simulation_data_ratio$N.Total.plots_mean)/population_6$N)
+		population_6$CACA_on_Stricta_ratio_var/unique(temp_sim_data_ratio$N.Total.plots_mean) *
+		(1 - unique(temp_sim_data_ratio$N.Total.plots_mean)/population_6$N)
 	) /	mean_MSE_CACA_on_Stricta
 	RE_MEPR_on_Stricta = (
-		population_6$MEPR_on_Stricta_ratio_var/unique(simulation_data_ratio$N.Total.plots_mean) *
-		(1 - unique(simulation_data_ratio$N.Total.plots_mean)/population_6$N)
+		population_6$MEPR_on_Stricta_ratio_var/unique(temp_sim_data_ratio$N.Total.plots_mean) *
+		(1 - unique(temp_sim_data_ratio$N.Total.plots_mean)/population_6$N)
 	) /	mean_MSE_MEPR_on_Stricta
 	RE_Old_Moth_Evidence_Stricta = (
-		population_6$Old_Moth_Evidence_Stricta_ratio_var/unique(simulation_data_ratio$N.Total.plots_mean) *
-		(1 - unique(simulation_data_ratio$N.Total.plots_mean)/population_6$N)
+		population_6$Old_Moth_Evidence_Stricta_ratio_var/unique(temp_sim_data_ratio$N.Total.plots_mean) *
+		(1 - unique(temp_sim_data_ratio$N.Total.plots_mean)/population_6$N)
 	) /	mean_MSE_Old_Moth_Evidence_Stricta
 	# CALCULATE MSE and RE USING FUNCTIONS
 	example_bias = calculateSamplingBias(
 		population_data_summary	= population_6, 
-		simulation_data = simulation_data, 
+		temp_sim_data = temp_sim_data, 
 		sampling.grouping.variables	= c("N.Total.plots_mean", "N.SRSWOR.plots", 
 			"SamplingDesign"), 
 		population.grouping.variables = "population",
@@ -268,12 +269,12 @@ test_that("Sampling Bias and Relative Efficiency, population 1, SamplingDesign=A
 	)
 	population_1 <- patch_data_summary_wide %>% filter(population==1)
 	# MANUALLY CALCULATE MSE and RE
-	simulation_data <- simdata_all_re %>% filter(
+	temp_sim_data <- simulation_data %>% filter(
 		population==1, 
 		SamplingDesign=="ACS", 
 		N.SRSWOR.plots==40
 	)
-	simulation_data_ratio <- simdata_all_re %>% filter(
+	temp_sim_data_ratio <- simulation_data %>% filter(
 		population==1, 
 		SamplingDesign=="ACS", 
 		N.SRSWOR.plots==40,
@@ -283,60 +284,60 @@ test_that("Sampling Bias and Relative Efficiency, population 1, SamplingDesign=A
 	mean_MSE_Stricta <- sum(
 		(
 			# observed
-			simulation_data$Stricta_mean_observed - 
+			temp_sim_data$Stricta_mean_observed - 
 			# true
 			population_1$Stricta_mean
 		)^2
 	# n simulations
-	)/dim(simulation_data)[1]
+	)/dim(temp_sim_data)[1]
 	RE_Stricta = (
-		population_1$Stricta_var/unique(simulation_data$N.Total.plots_mean) *
-		(1 - unique(simulation_data$N.Total.plots_mean)/population_1$N)
+		population_1$Stricta_var/unique(temp_sim_data$N.Total.plots_mean) *
+		(1 - unique(temp_sim_data$N.Total.plots_mean)/population_1$N)
 	) /	mean_MSE_Stricta
 	# ratio variables
 	mean_MSE_CACA_on_Stricta <- sum(
 		(
 			# observed
-			simulation_data_ratio$CACA_on_Stricta_ratio_mean_observed - 
+			temp_sim_data_ratio$CACA_on_Stricta_ratio_mean_observed - 
 			# true
 			population_1$CACA_on_Stricta_ratio_mean
 		)^2
 	# n simulations
-	)/dim(simulation_data_ratio)[1]
+	)/dim(temp_sim_data_ratio)[1]
 	mean_MSE_MEPR_on_Stricta <- sum(
 		(
 			# observed
-			simulation_data_ratio$MEPR_on_Stricta_ratio_mean_observed - 
+			temp_sim_data_ratio$MEPR_on_Stricta_ratio_mean_observed - 
 			# true
 			population_1$MEPR_on_Stricta_ratio_mean
 		)^2
 	# n simulations
-	)/dim(simulation_data_ratio)[1]
+	)/dim(temp_sim_data_ratio)[1]
 	mean_MSE_Old_Moth_Evidence_Stricta <- sum(
 		(
 			# observed
-			simulation_data_ratio$Old_Moth_Evidence_Stricta_ratio_mean_observed - 
+			temp_sim_data_ratio$Old_Moth_Evidence_Stricta_ratio_mean_observed - 
 			# true
 			population_1$Old_Moth_Evidence_Stricta_ratio_mean
 		)^2
 	# n simulations
-	)/dim(simulation_data_ratio)[1]
+	)/dim(temp_sim_data_ratio)[1]
 	RE_CACA_on_Stricta = (
-		population_1$CACA_on_Stricta_ratio_var/unique(simulation_data_ratio$N.Total.plots_mean) *
-		(1 - unique(simulation_data_ratio$N.Total.plots_mean)/population_1$N)
+		population_1$CACA_on_Stricta_ratio_var/unique(temp_sim_data_ratio$N.Total.plots_mean) *
+		(1 - unique(temp_sim_data_ratio$N.Total.plots_mean)/population_1$N)
 	) /	mean_MSE_CACA_on_Stricta
 	RE_MEPR_on_Stricta = (
-		population_1$MEPR_on_Stricta_ratio_var/unique(simulation_data_ratio$N.Total.plots_mean) *
-		(1 - unique(simulation_data_ratio$N.Total.plots_mean)/population_1$N)
+		population_1$MEPR_on_Stricta_ratio_var/unique(temp_sim_data_ratio$N.Total.plots_mean) *
+		(1 - unique(temp_sim_data_ratio$N.Total.plots_mean)/population_1$N)
 	) /	mean_MSE_MEPR_on_Stricta
 	RE_Old_Moth_Evidence_Stricta = (
-		population_1$Old_Moth_Evidence_Stricta_ratio_var/unique(simulation_data_ratio$N.Total.plots_mean) *
-		(1 - unique(simulation_data_ratio$N.Total.plots_mean)/population_1$N)
+		population_1$Old_Moth_Evidence_Stricta_ratio_var/unique(temp_sim_data_ratio$N.Total.plots_mean) *
+		(1 - unique(temp_sim_data_ratio$N.Total.plots_mean)/population_1$N)
 	) /	mean_MSE_Old_Moth_Evidence_Stricta
 	# CALCULATE MSE and RE USING FUNCTIONS
 	example_bias = calculateSamplingBias(
 		population_data_summary	= population_1, 
-		simulation_data = simulation_data, 
+		temp_sim_data = temp_sim_data, 
 		sampling.grouping.variables	= c("N.Total.plots_mean", "N.SRSWOR.plots", 
 			"SamplingDesign"), 
 		population.grouping.variables = "population",
